e6fd628e4a11cb5f23343db235339c929479ff6d
ARSHADMSC <91111692+ARSHADMSC@users.noreply.github.com>
2022-03-29 22:49:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import './App.css'
+import {useState} from 'react'
+function App() {
+    const [toDos,setToDos] = useState([])
+    const [toDo,setToDo] = useState('')
+    return (
+      <div className="app">
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+          <i  onClick={()=>setToDos([...toDos,{id:Date.now(),text: toDo,status:false}])} className="fas fa-plus"></i>
+        </div>
+        <div className="todos">
+         {
+            toDos.map((value)=>{
+            return(
+            <div className="todo">
+            <div className="left">
+              <input onChange={(e)=>{
+                console.log(e.target.checked)
+                console.log(value)
+                setToDos(toDos.filter(obj=>{
+                  if(obj.id===value.id){
+                    obj.status=e.target.checked
+                  }
+                  return obj;
+                }))
+            }}
+              value={value.status} type="checkbox" name="" id="" />
+              <p>{value.text}</p>
+            <div className="right">
+              <i className="fas fa-times"></i>
+              </div>
+            </div>
+            </div>
+          )
+         })
+        }
+      </div> 
+      </div>
+      );
+    }
+export default App
